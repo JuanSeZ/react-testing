841f06d19b189536930446df46915c6e3d314b80
JuanSeZ <juan.zanelli@ing.austral.edu.ar>
2024-03-22 21:37:30 -0300

    add password tab validation and tests

--- src/settings/tabs/PasswordTab.jsx
+++ src/settings/tabs/PasswordTab.jsx
@@ -4,39 +4,97 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {z} from "zod";
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form.jsx";
+import {useToast} from "@/components/ui/use-toast.js";
+
+
+const formSchema = z.object({
+  new: z.string().min(6),
+  'new-repeated': z.string().min(6)
+}).refine(data => data.new === data['new-repeated'], {
+  message: "New password and repeated password must match",
+  path: ['new-repeated']
+});
+
 
 export const PasswordTab = () => {
+	const { toast } = useToast()
+
+	const form = useForm({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			current: 'secret-password',
+			new: '',
+			'new-repeated': ''
+		}
+	})
+	const onSubmit = (values) => {
+		console.log({ values })
+		toast({ title: 'Success', text: 'Password changed' })
+	}
+
 	return (
 		<Card>
 			<CardHeader>
 				<CardTitle>Password</CardTitle>
 				<CardDescription>
-					Change your password here. After saving, you'll be logged out.
+					Change your password here. After saving, you`ll be logged out.
 				</CardDescription>
 			</CardHeader>
 			<CardContent className="space-y-2">
-				<div className="space-y-1">
-					<Label htmlFor="current">Current password</Label>
-					<Input id="current" type="password" />
-				</div>
-				<div className="space-y-1">
-					<Label htmlFor="new">New password</Label>
-					<Input id="new" type="password" />
-				</div>
-				<div className="space-y-1">
-					<Label htmlFor="new-repeated">Repeat new password</Label>
-					<Input id="new-repeated" type="password" />
-				</div>
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+						<FormField
+							control={form.control}
+							name="current"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Current password</FormLabel>
+									<FormControl>
+										<Input placeholder='current' id="current" type="password" {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="new"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>New password</FormLabel>
+									<FormControl>
+										<Input placeholder='new'  id="new" type="password" {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="new-repeated"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Repeat new password</FormLabel>
+									<FormControl>
+										<Input placeholder='new-repeated' id="new-repeated" type="password" {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<Button type="submit">Save password</Button>
+					</form>
+				</Form>
 			</CardContent>
-			<CardFooter>
-				<Button>Save password</Button>
-			</CardFooter>
 		</Card>
 	)
 }
+
--- src/settings/tabs/PasswordTab.spec.jsx
+++ src/settings/tabs/PasswordTab.spec.jsx
@@ -13,7 +13,64 @@
 // - Text: `Password changed`.
 
 // 3) Add tests aiming to cover all the functionality.
+import React from 'react';
+import { describe, test, expect, vi } from 'vitest';
+import {fireEvent, render, waitFor} from "@testing-library/react";
+import {PasswordTab} from "@/settings/tabs/PasswordTab.jsx";
+import {screen} from "@/test/index.js";
 
-import { describe } from 'vitest';
+const mockToast = vi.fn();
+vi.mock('@/components/ui/use-toast.js', () => ({
+    useToast: vi.fn(() => ({ toast: mockToast }))
+}))
 
-describe.skip('PasswordTab', () => {})
+describe('PasswordTab', () => {
+
+    test('should match snapshot', () => {
+        const { container } = render(<PasswordTab />)
+        expect(container.firstChild).toMatchSnapshot()
+    })
+
+    test('submitting calls console.log with expected arguments', async () => {
+        const consoleSpy = vi.spyOn(console, 'log')
+        const { getByPlaceholderText} = render(<PasswordTab />)
+        fireEvent.change(getByPlaceholderText('new'), { target: { value: 'new-secret-password' } })
+        fireEvent.change(getByPlaceholderText('new-repeated'), { target: { value: 'new-secret-password' } })
+        fireEvent.click(screen.getByRole('button', { name: 'Save password' }))
+        await waitFor(() => {
+
+            expect(consoleSpy).toHaveBeenCalledWith({
+                values: {
+                    new: 'new-secret-password', 'new-repeated': 'new-secret-password'
+                }
+            })
+
+        })
+    })
+
+    test('should show error when new password is equal to current password', async () => {
+        const { getByPlaceholderText} = render(<PasswordTab />)
+        fireEvent.change(getByPlaceholderText('new'), { target: { value: 'secret-password' } })
+        fireEvent.change(getByPlaceholderText('new-repeated'), { target: { value: 'not-same-password' } })
+        fireEvent.click(screen.getByRole('button', { name: 'Save password' }))
+        await waitFor(() => {
+            expect(screen.getByText('New password and repeated password must match')).toBeInTheDocument()
+        })
+    })
+
+    test('should show a toast with title `Success` and text `Password changed` when form is submitted', async () => {
+        const { getByPlaceholderText } = render(<PasswordTab />)
+
+        fireEvent.change(getByPlaceholderText('new'), { target: { value: 'new-secret-password' } })
+        fireEvent.change(getByPlaceholderText('new-repeated'), { target: { value: 'new-secret-password' } })
+        fireEvent.click(screen.getByRole('button', { name: 'Save password' }))
+
+        await waitFor(() => {
+            expect(mockToast).toHaveBeenCalledWith({
+                title: 'Success',
+                text: 'Password changed'
+            })
+        })
+    })
+
+})
